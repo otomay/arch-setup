6a3f0278f48e368a5f7c7b4533b47354edfa2ce3
otomay <renanalvesgomes@gmail.com>
2025-09-20 18:22:53 -0300

    add mais pacotes

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ sudo pacman -S ttf-dejavu noto-fonts noto-fonts-emoji ttf-liberation
 sudo pacman -S gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer
 sudo pacman -S hyprland kitty
 sudo pacman -S xdg-desktop-portal xdg-desktop-portal-hyprland
+sudo pacman -S zip unzip tar
+sudo pacman -S --neded dolphin firefox flatpak
+sudo pacman -S intel-ucode
+sudo pacman -S wofi
