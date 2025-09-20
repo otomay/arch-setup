18acf33349236a42aa318bd27ba5fb329d144b51
otomay <renanalvesgomes@gmail.com>
2025-09-20 15:22:10 -0300

    initial files

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo pacman -Syu
+sudo pacman -S ttf-dejavu noto-fonts noto-fonts-emoji ttf-liberation
+sudo pacman -S gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer
+sudo pacman -S hyprland kitty
+sudo pacman -S xdg-desktop-portal xdg-desktop-portal-hyprland
