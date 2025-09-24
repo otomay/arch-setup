d14c69c193c85b2cae695091393f11712b2f2570
otomay <54224340+otomay@users.noreply.github.com>
2025-09-24 17:29:21 -0300

    update!

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo pacman -S --noconfirm ttf-font-awesome ttf-jetbrains-mono-nerd ttf-droid \
     ttf-roboto ttf-dejavu noto-fonts noto-fonts-emoji ttf-liberation
 
 sudo pacman -S --noconfirm gst-libav gst-plugins-base gst-plugins-bad gst-plugins-good \
-    gst-plugins-ugly ffmpeg gstreamer
+    gst-plugins-ugly ffmpeg gstreamer neovim
 
 sudo pacman -S --noconfirm hyprland hyprlock hypridle hyprcursor hyprpaper hyprpicker waybar kitty
 
@@ -30,5 +30,20 @@ makepkg -si --noconfirm
 cd .. && rm -rf yay
 
 yay -S --noconfirm hyprshot wlogout qview visual-studio-code-bin brave-bin qt5ct-kde qt6ct-kde
+
+# ================ lazyvim
+
+# required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
 
 # systemctl --user enable pipewire pipewire-pulse wireplumber
