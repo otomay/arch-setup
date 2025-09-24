ecfd9d2dd8869b3b5f53633a558cffd36e36ac1e
otomay <54224340+otomay@users.noreply.github.com>
2025-09-24 18:01:21 -0300

    add zsh

--- install.sh
+++ install.sh
@@ -44,6 +44,16 @@ mv ~/.cache/nvim{,.bak}
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
+sudo pacman -S zsh-autosuggestions zsh-syntax-highlighting zsh -y
+
+# Install Oh My ZSH
+echo "Installing Oh My ZSH..."
 sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+chsh -s $(which zsh)
 
 # systemctl --user enable pipewire pipewire-pulse wireplumber
