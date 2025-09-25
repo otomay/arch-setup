2a0a2a6f084505eb64f314a2c31ef0a9821001c4
otomay <54224340+otomay@users.noreply.github.com>
2025-09-25 18:03:16 -0300

    add pkglist

--- install.sh
+++ install.sh
@@ -3,33 +3,10 @@ set -e
 
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S --noconfirm ttf-font-awesome ttf-jetbrains-mono-nerd ttf-droid \
-    ttf-roboto ttf-dejavu noto-fonts noto-fonts-emoji ttf-liberation
-
-sudo pacman -S --noconfirm gst-libav gst-plugins-base gst-plugins-bad gst-plugins-good \
-    gst-plugins-ugly ffmpeg gstreamer neovim
-
-sudo pacman -S --noconfirm hyprland hyprlock hypridle hyprcursor hyprpaper hyprpicker waybar kitty
-
-sudo pacman -S --noconfirm xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
-
-sudo pacman -S --noconfirm zip unzip tar dunst cliphist mpv pavucontrol xdg-user-dirs-gtk
-
-sudo pacman -S --noconfirm --needed dolphin dolphin-plugins ark kio-admin polkit-kde-agent gnome-keyring \
-    qt5-wayland qt6-wayland firefox flatpak
-
-sudo pacman -S --noconfirm breeze breeze5 breeze-gtk papirus-icon-theme nwg-look
-
-sudo pacman -S --noconfirm intel-ucode
-
-sudo pacman -S --noconfirm rofi-wayland
-
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 cd .. && rm -rf yay
-
-yay -S --noconfirm hyprshot wlogout qview visual-studio-code-bin brave-bin qt5ct-kde qt6ct-kde
 
 # ================ lazyvim
 
@@ -44,7 +21,7 @@ mv ~/.cache/nvim{,.bak}
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
-sudo pacman -S zsh-autosuggestions zsh-syntax-highlighting zsh -y
+# ================ oh my zsh
 
 # Install Oh My ZSH
 echo "Installing Oh My ZSH..."
--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -16,3 +16,9 @@ cp -f ~/.oh-my-zsh/themes/bira.zsh-theme dotfiles/zsh/.oh-my-zsh/themes/
 for d in gtk-3.0 gtk-4.0 hypr hyprpanel kitty qt5ct qt6ct rofi; do
     cp -rf ~/.config/$d dotfiles/config/
 done
+
+# ===
+
+mkdir -p pkglist
+pacman -Qqe > pkglist/pacman-packages.txt
+yay -Qqe > pkglist/yay-packages.txt
