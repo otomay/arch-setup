fb5e382809a57c71db67c77cf3c522be081138ff
otomay <54224340+otomay@users.noreply.github.com>
2026-01-22 14:37:08 -0300

    Backup automático 2026-01-22 14:37:08

--- dotfiles/config/hypr/scripts/display-toggle.sh
+++ dotfiles/config/hypr/scripts/display-toggle.sh
@@ -11,11 +11,13 @@ hdmi_connected=$(hyprctl monitors all | grep -q "HDMI-A-1 (ID" && echo true || e
 if [[ "$hdmi_connected" == true ]]; then
     if hyprctl monitors | grep -q "eDP-1 (ID"; then
         log "Desativando eDP-1 e ativando HDMI-A-1."
-        hyprctl keyword monitor "eDP-1,disable"
-        hyprctl keyword monitor "HDMI-A-1,1920x1080@240,0x0,1"
+        #hyprctl keyword monitor "eDP-1,disable"
+        sleep 1
+        #hyprctl keyword monitor "HDMI-A-1,1920x1080@240,0x0,1"
     else
         log "Ativando eDP-1 e desativando HDMI-A-1."
-        hyprctl keyword monitor "eDP-1,1920x1080@60,1920x0,1.25"
-        hyprctl keyword monitor "HDMI-A-1,disable"
+        #hyprctl keyword monitor "eDP-1,1920x1080@60,1920x0,1.25"
+        sleep 1
+        #hyprctl keyword monitor "HDMI-A-1,disable"
     fi
 fi
--- dotfiles/config/hypr/scripts/toggle-monitor.sh
+++ dotfiles/config/hypr/scripts/toggle-monitor.sh
@@ -32,25 +32,27 @@ fi
 # Aplica lógica
 if [[ "$lid_closed" == true ]]; then
     log "Desligando eDP-1 por causa da tampa fechada."
-    hyprctl keyword monitor "eDP-1,disable"
 
     if [[ "$hdmi_connected" == true ]]; then
         log "Ativando HDMI-A-1."
+        hyprctl keyword monitor "eDP-1,disable"
+        sleep 1
         hyprctl keyword monitor "HDMI-A-1,1920x1080@240,0x0,1"
     else
         log "HDMI-A-1 não conectado. Nenhum monitor ativo!"
     fi
-
     exit 0
 fi
 
 # Se tampa aberta, fazer toggle normal entre eDP-1 e HDMI-A-1
-if hyprctl monitors | grep -q "eDP-1 (ID"; then
-    log "Desativando eDP-1 e ativando HDMI-A-1."
-    hyprctl keyword monitor "eDP-1,disable"
-    hyprctl keyword monitor "HDMI-A-1,1920x1080@240,0x0,1"
-else
-    log "Ativando eDP-1 e desativando HDMI-A-1."
-    hyprctl keyword monitor "eDP-1,1920x1080@60,0x0,1.25"
-    hyprctl keyword monitor "HDMI-A-1,disable"
+if [[ "$hdmi_connected" == true ]]; then
+    if hyprctl monitors | grep -q "eDP-1 (ID"; then
+        log "Desativando eDP-1 e ativando HDMI-A-1."
+        hyprctl keyword monitor "eDP-1,disable"
+        hyprctl keyword monitor "HDMI-A-1,1920x1080@240,0x0,1"
+    else
+        log "Ativando eDP-1 e desativando HDMI-A-1."
+        hyprctl keyword monitor "eDP-1,1920x1080@60,0x0,1.25"
+        hyprctl keyword monitor "HDMI-A-1,disable"
+    fi
 fi
